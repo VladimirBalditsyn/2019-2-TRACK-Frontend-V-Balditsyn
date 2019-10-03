37ff0b89ba9f4323dadbaabf280e73cb47275a59
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-03 08:14:17 +0000

    Deploy VladimirBalditsyn/2019-2-TRACK-Frontend-V-Balditsyn to github.com/VladimirBalditsyn/2019-2-TRACK-Frontend-V-Balditsyn.git:gh-pages

---
+++ precache-manifest.8c0e5cf5a6a622d5ba5f81a30974ce94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae347ee8eb6a8e9c0bdc37d0858a6e2",
+    "url": "/VladimirBalditsyn/2019-2-TRACK-Frontend-V-Balditsyn/index.html"
+  },
+  {
+    "revision": "1f758e1ce90363bfee48",
+    "url": "/VladimirBalditsyn/2019-2-TRACK-Frontend-V-Balditsyn/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "10c359d24f98af51aa8d",
+    "url": "/VladimirBalditsyn/2019-2-TRACK-Frontend-V-Balditsyn/static/js/2.12c7c0f2.chunk.js"
+  },
+  {
+    "revision": "1f758e1ce90363bfee48",
+    "url": "/VladimirBalditsyn/2019-2-TRACK-Frontend-V-Balditsyn/static/js/main.c617ed80.chunk.js"
+  },
+  {
+    "revision": "73918bb863d3fb71928d",
+    "url": "/VladimirBalditsyn/2019-2-TRACK-Frontend-V-Balditsyn/static/js/runtime-main.d0052515.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "/VladimirBalditsyn/2019-2-TRACK-Frontend-V-Balditsyn/static/media/logo.795fb555.svg"
+  }
+]);
